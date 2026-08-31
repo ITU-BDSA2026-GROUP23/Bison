d8b90027be55edce16410ad0e2b26a66a4d07239
Marius Nunn <mnun@itu.dk>
2026-08-31 11:41:53 +0200

    Reading CSV file

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string CSVPath = "./bison_observe_cli_db.csv";
+StreamReader reader = new StreamReader(CSVPath);
+reader.ReadLine(); // Skip the header line
+while(!reader.EndOfStream)
+{
+    string line = reader.ReadLine();
+    string author = line.Split(',')[0]; 
+    string observation = line.Split(',')[1].Replace("\"", ""); 
+    string timestamp = line.Split(',')[2];  
+
+    Console.WriteLine($"{author} @ {timestamp}: {observation}");
+}
+
