13f060142c76c1c0792bc186a760f175f705dcc4
Nikolai Gallego Patino <ngal@itu.dk>
2026-09-13 15:50:31 +0200

    Added unit test for rejecting comments on non-existing observations. Co-authored-by: Emilia Odegaard <eoed@itu.dk>

---
+++ test/Bison.CLI.Tests/CommentServiceTests.cs
@@ -0,0 +1,27 @@
+using SimpleDB;
+
+namespace Bison.CLI.Tests;
+
+public class CommentServiceTests
+{
+    [Fact]
+    public void TryAddComment_OnNonExistingObservation_IsRejected()
+    {
+       string observePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid + ".csv"); 
+       string commentPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid + ".csv"); 
+
+       var dbObserve = new CSVDatabase<ObserveCheep>(observePath);
+       var dbComment = new CSVDatabase<CommentCheep>(commentPath);
+
+       var commentService = new CommentService(dbObserve, dbComment);
+
+       bool result = commentService.TryAddComment("emili", "nice bird", 99999, out var error);
+
+       Assert.False(result);
+       Assert.NotNull(error);
+
+
+    }
+}
+
+    
--- test/Bison.CLI.Tests/obj/Debug/net8.0/Bison.CLI.Tests.AssemblyInfo.cs
+++ test/Bison.CLI.Tests/obj/Debug/net8.0/Bison.CLI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bison.CLI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f540decb8e4dd08edee03b9d5aa2c1ecac6c773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4059582c4bec34dcb558540ca6f90727766319ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bison.CLI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bison.CLI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
